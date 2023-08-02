aa7b5545d9cd61102bad439bca6b18851299b0b3
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-02 22:24:35 +0100

    Mapty Class Inheritance

--- script.js
+++ script.js
@@ -13,42 +13,96 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 ////////
 
-// GeoLocation API
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+// Implementing App class
+class App {
+  // Declaring the global variables in a class
+  #map; //This is how to declare it, as a private property
+  #mapEvent;
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap',
-      }).addTo(map);
+  constructor() {
+    // Constructor method is called immediately when a new object is created from its class.
+    this._getPosition();
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
+    // Render and submit workout form
+    form.addEventListener('submit', this._newWorkout.bind(this)); //If the method (newWorkout) is not binded to the 'this' keyword,...
+    // ...the 'this' keyword will point to 'form'...
+    //...and that's not what we want. We want the 'this' keyword to point to the 'class App' (APP Object).
+    //When working with eventListeners in classes, you need to always bind the this keyword.
 
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup'
-          }))
-          .setPopupContent('Workout')
-          .openPopup();
-      });
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
+    // Switching between Cadence and Elevation
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-console.log(firstName);
+  _getPosition() {
+    if (navigator.geolocation)
+      // Get Current location coordinates
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    //   Render Map on current location
+    const coords = [latitude, longitude];
+
+    // console.log(this);
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(this.#map);
+
+    //  Handling User clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus(); //First input form to fill immediately
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    // Display workout on map
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App(); //app object created from App class, and its created immediately when the page loads.
